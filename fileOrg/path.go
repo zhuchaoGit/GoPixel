3e4e81fc53f765a0396755e14a2fda22afb31fee
zhuchao <zhuchao@benmu-health.com>
2018-07-03 15:35:54 +0800

    commit

--- fileOrg/path.go
+++ fileOrg/path.go
@@ -1,5 +1,11 @@
 package fileOrg
 
+func TemplatePath() string {
+	return "asserts/html/index_0000000000.html"
+}
+func TemplateFile() string {
+	return "index_0000000000.html"
+}
 func ImgPath() string {
 	return "asserts/img"
 }
